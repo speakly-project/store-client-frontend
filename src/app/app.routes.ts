b32b03c1f985187f9fbdb0517cb0d8948119e9ab
Alberto Rubio F.dez <alberto13101@gmail.com>
2026-01-19 12:58:30 +0100

    refactor: Cambiado el desplegable del header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { loginGuard } from './guards/login-guard';
 import { Home } from './components/pages/home/home';
 
 export const routes: Routes = [
-    { path: '', component: Home, canActivate: [loginGuard] },
+    { path: '', component: Home },
     { path: 'login', component: Login},
     { path: 'logout', component: Logout },
     { path: '**', redirectTo: '' },
